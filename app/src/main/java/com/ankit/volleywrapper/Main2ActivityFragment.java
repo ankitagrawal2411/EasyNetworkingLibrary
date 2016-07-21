6f215d2a00f4b4fc07ebb217035286a310e34919
ankitagrawal <ankit.agrawal@firstcry.com>
2016-07-21 18:31:03 +0530

    fixed a bug

--- app/src/main/java/com/ankit/volleywrapper/Main2ActivityFragment.java
+++ app/src/main/java/com/ankit/volleywrapper/Main2ActivityFragment.java
@@ -32,45 +32,7 @@ public class Main2ActivityFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         GlobalRequest.newBuilder().setRequestManager(new VolleyRequestHandler(getActivity()));
-        TelephonyManager telephonyManager = (TelephonyManager) getActivity()
-                .getSystemService(Context.TELEPHONY_SERVICE);
-        JSONObject params = new JSONObject();
-        try {
-            params.put("XBkey", "XB123");
-            params.put("UserID", "7750");
-            params.put("Password", "123");
-            params.put("DeviceID", telephonyManager.getDeviceId());
-            params.put("AppType", "UA");
-            params.put("AppVersion", "0.6");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        //for post request
-        new RequestBuilder().post(params).url("http://192.168.0.51:804/LogisticAndroidService.svc/DeliveryUserValidation").tag("tag").asGsonObject(LoginModel.class,new
-                                                                                                                                                           IRequestListener<LoginModel>() {
 
-
-            @Override
-            public Object onRequestSuccess(LoginModel response) {
-                return null;
-            }
-
-            @Override
-            public void onParseSuccess(Object response) {
-
-            }
-
-            @Override
-            public LoginModel onNetworkResponse(NetworkResponse response) {
-                return null;
-            }
-
-            @Override
-            public void onRequestErrorCode(int errorCode) {
-
-            }
-        }).send
-                (getActivity());
 
     }
 }
